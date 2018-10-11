4617e3014e29be801fbdd847125380b8ef9afcf9
Jakub Giminski <jakubgiminski@gmail.com>
2018-10-11 09:38:32 +0100

    Add more factory methods to Type

--- src/Type.php
+++ src/Type.php
@@ -24,22 +24,37 @@ class Type
         return new self('array', 'array');
     }
 
+    public static function integer(): self
+    {
+        return new self('scalar', 'integer');
+    }
+
+    public static function string(): self
+    {
+        return new self('scalar', 'string');
+    }
+
+    public static function float(): self
+    {
+        return new self('scalar', 'float');
+    }
+
     public static function scalar(string $type): self
     {
         return new self('scalar', $type);
     }
 
-    public function isObjectCollection(): bool
+    public function isObject(): bool
     {
         return $this->kind === 'object';
     }
 
-    public function isArrayCollection(): bool
+    public function isArray(): bool
     {
         return $this->kind === 'array';
     }
 
-    public function isScalarCollection(): bool
+    public function isScalar(): bool
     {
         return $this->kind === 'scalar';
     }
@@ -51,16 +66,16 @@ class Type
 
     public function validate($element): void
     {
-        if ($this->isObjectCollection() && !$element instanceof $this->type) {
+        if ($this->isObject() && !$element instanceof $this->type) {
             $type = is_object($element) ? get_class($element) : gettype($element);
             throw InvalidTypeException::create($this->type, $type);
         }
 
-        if ($this->isArrayCollection() && !is_array($element)) {
+        if ($this->isArray() && !is_array($element)) {
             throw InvalidTypeException::create('array', gettype($element));
         }
 
-        if ($this->isScalarCollection() && gettype($element) !== $this->type) {
+        if ($this->isScalar() && gettype($element) !== $this->type) {
             throw InvalidTypeException::create($this->type, gettype($element));
         }
     }
